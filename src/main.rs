484293150a52b692ecb30cb5899b70a7211faa33
Denis Klintukhov <35447261+DKlintukhov@users.noreply.github.com>
2025-06-20 07:09:16 +0300

    Add: dotenv lib (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let key = env::var("API_KEY").expect("API_KEY must be set");
+    let url = env::var("BASE_URL").expect("BASE_URL must be set");
+    println!("ENV: {key} {url}");
 }
