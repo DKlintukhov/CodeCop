1e7d051f44af3543468ff044cee0a9e0d5458c4c
Denis Klintukhov <denis.klintukhov@gmail.com>
2025-06-28 17:05:21 +0300

    Add: App config

--- src/app_config.rs
+++ src/app_config.rs
@@ -17,24 +17,8 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-mod app_config;
 
-use dotenv::dotenv;
-use openai::{
-    Credentials,
-    chat::{ChatCompletionMessage, ChatCompletionMessageRole},
-};
-
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-
-    let credentials = Credentials::from_env();
-
-    let messages = vec![ChatCompletionMessage {
-        role: ChatCompletionMessageRole::System,
-        content: Some("You are a large language model built into a command line interface as an example of what the `openai` Rust library made by Valentine Briese can do.".to_string()),
-        ..Default::default()
-    }];
-    println!("{:?}, {:?}", credentials, messages);
+struct AppConfig {
+    url: String,
+    api_token: String,
 }
